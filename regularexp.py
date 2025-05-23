42287dfaa1d9e19269e4a0acc040ef106a64f6e5
Jagadeeshchiriki <153111425+Jagadeeshchiriki@users.noreply.github.com>
2025-05-23 17:14:21 +0530

    first commit

--- regularexp.py
+++ regularexp.py
@@ -2,10 +2,10 @@ import re
 data='''The power of casta living introduces 
     , ths sience of yogq ! and ... restore  @ 34 apr 20020 well-being to '''
 
-# pattern=re.compile(r'\w')
-# result= pattern.finditer(data)
-# for i in (list(result)):
-#     print(i)
+pattern=re.compile(r'\w')
+result= pattern.finditer(data)
+for i in (list(result)):
+    print(i)
 
 
 
@@ -15,35 +15,35 @@ data='''The power of casta living introduces
 
 # match --> find the staring position with using the re
 # to start the matching at intial position , return singel object
-# matchobj = re.match(r'.+',data)   # '.' ,'\n' , '\d','\D', '\w', '\W' ,'@', '\s', '\S'
-# print(matchobj)
-# if matchobj:
-#     print("matchobj.group() : ",matchobj.group(0))
-# else:
-#     print("no match!")
+matchobj = re.match(r'.+',data)   # '.' ,'\n' , '\d','\D', '\w', '\W' ,'@', '\s', '\S'
+print(matchobj)
+if matchobj:
+    print("matchobj.group() : ",matchobj.group(0))
+else:
+    print("no match!")
 
 
 # search - search only one time at any where match
 # return singe object
-# matchobj=re.search(r' .* \d',data)
-# print(matchobj)
-# if matchobj:
-#     print("matchobj.group() : ",matchobj.group(0))
-# else:
-#     print("no match!")
+matchobj=re.search(r' .* \d',data)
+print(matchobj)
+if matchobj:
+    print("matchobj.group() : ",matchobj.group(0))
+else:
+    print("no match!")
 
 # find all -- find how many time the pattern match 
 # return multiple object
-# matchobj=re.findall(r'\d\d',data)
-# print(matchobj)
-# print(len(matchobj))
+matchobj=re.findall(r'\d\d',data)
+print(matchobj)
+print(len(matchobj))
 
 
 #  replace(old, new)
 matchobj=re.findall(r'\S',data)
 print(matchobj)
 # data =data.replace(r'casta','02')
-# print(data)
+print(data)
 
-# data= re.sub('casting','02',data)
-# print(data)
+data= re.sub('casting','02',data)
+print(data)
