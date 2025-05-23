becc8f40b4b966c97ffbc137abe1f8406f7be2ca
Jagadeeshchiriki <153111425+Jagadeeshchiriki@users.noreply.github.com>
2025-05-23 17:07:33 +0530

    numpy

--- num.py
+++ num.py
@@ -1,48 +1,48 @@
 import numpy as np
-# print(np.__version__)
-# a=np.array([[10,20],[2,3]])
-# print(a[1][0])
+print(np.__version__)
+a=np.array([[10,20],[2,3]])
+print(a[1][0])
 
 
-# # asarray(input,datatype,order=c,f)
-# a=[[10,20],[30,40]]
-# b=np.asarray(a,dtype=float,order="F")
-# print(b)
-# # using the nditer method to print the asarray method
-# for i in np.nditer(b):
-#     print(i)
+# asarray(input,datatype,order=c,f)
+a=[[10,20],[30,40]]
+b=np.asarray(a,dtype=float,order="F")
+print(b)
+# using the nditer method to print the asarray method
+for i in np.nditer(b):
+    print(i)
 
-# # when you have a bytes, bytearray, or other buffer object.
-# # a consider a buffer then using the 'b' infront the string
+# when you have a bytes, bytearray, or other buffer object.
+# a consider a buffer then using the 'b' infront the string
 a=b"welcome to numpy"
 c=np.frombuffer(a,dtype="S1",count=10,offset=4)
 print(c)
 
-# # Creates an array from any iterable, like a list, generator, or range.
-# c=np.fromiter(a,dtype="S1")
-# print(c)
+# Creates an array from any iterable, like a list, generator, or range.
+c=np.fromiter(a,dtype="S1")
+print(c)
 
 # intializing array
 
 # zeros() filling all values with zeros
-# a=np.zeros([2,3])
-# print(a)
+a=np.zeros([2,3])
+print(a)
 
 # ones() filling all values with ones
-# a=np.ones([2,3])
-# print(a)
+a=np.ones([2,3])
+print(a)
 
 # eye() filling all diagnol values with ones in square matrix
-# a=np.eye(2)
-# print(a)
+a=np.eye(2)
+print(a)
 
 # full(dimention, element)
-# a=np.full(4,2)
-# print(a)
+a=np.full(4,2)
+print(a)
 
-# # random.rand(dimention)
-# a=np.random.rand(5,10)
-# print(a)
+# random.rand(dimention)
+a=np.random.rand(5,10)
+print(a)
 
 # array operations ----
 # accessing and slicing operations
@@ -58,20 +58,20 @@ print(a[1,0:2])
 
 
 # arange(start,end,step)
-# a=np.arange(10,70,10)
-# print(a)
+a=np.arange(10,70,10)
+print(a)
 
-# a=a.reshape(2,3)
-# print(a)
+a=a.reshape(2,3)
+print(a)
 
 # linespace(start,stop,step,edpoint=t,f)
-# a=np.linspace(10,90,10,endpoint=False,retstep=True,dtype=int)
-# print(a)
+a=np.linspace(10,90,10,endpoint=False,retstep=True,dtype=int)
+print(a)
 
 # logspace(10,20,5,base)
-# a=np.logspace(10,20,5)
-# print(a)
+a=np.logspace(10,20,5)
+print(a)
 
-# a=([[10,30,50],[23,12,19]])
-# a=np.sort(a,axis=0)
-# print(a)
+a=([[10,30,50],[23,12,19]])
+a=np.sort(a,axis=0)
+print(a)
